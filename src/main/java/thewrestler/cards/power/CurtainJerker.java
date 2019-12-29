262a21b90521f8bfcabfd59e3c25e35b77b76c79
Justin Woolley <19597672+jwoolley@users.noreply.github.com>
2019-12-29 13:43:19 -0800

    Curtain Jerker to uncommon

--- src/main/java/thewrestler/cards/power/CurtainJerker.java
+++ src/main/java/thewrestler/cards/power/CurtainJerker.java
@@ -27,7 +27,7 @@ public class CurtainJerker extends CustomCard {
   private static final CardStrings cardStrings;
 
   private static final CardType TYPE = CardType.POWER;
-  private static final CardRarity RARITY = CardRarity.RARE;
+  private static final CardRarity RARITY = CardRarity.UNCOMMON;
   private static final CardTarget TARGET = CardTarget.NONE;
 
   private static final int COST = 1;
