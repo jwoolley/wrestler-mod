90d5c99a3a6c8be83bd39dacf5cb7396f8c5f2f1
Justin Woolley <19597672+jwoolley@users.noreply.github.com>
2020-01-12 21:54:37 -0800

    card tweak: Frog Splash

--- src/main/java/thewrestler/cards/attack/FrogSplash.java
+++ src/main/java/thewrestler/cards/attack/FrogSplash.java
@@ -31,7 +31,7 @@ public class FrogSplash extends CustomCard {
   private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
 
   private static final int COST = 2;
-  private static final int DAMAGE = 12;
+  private static final int DAMAGE = 10;
   private static final int DAMAGE_UPGRADE = 4;
   private static final int WEAK_AMOUNT = 1;
   private static final int WEAK_AMOUNT_UPGRADE = 1;
